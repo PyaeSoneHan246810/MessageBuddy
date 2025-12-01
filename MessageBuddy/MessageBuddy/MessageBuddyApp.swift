f9497f25b4f6350f67908277fa7abc659b5cf79c
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-12-01 16:00:38 +0700

    [Chore] Create SwiftUI project, configure minimum deployments, identity, and deployment info settings for the project

---
+++ MessageBuddy/MessageBuddy/MessageBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessageBuddyApp.swift
+//  MessageBuddy
+//
+//  Created by Dylan on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MessageBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
